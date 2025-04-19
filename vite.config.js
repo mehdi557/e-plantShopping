c73995387074097bdb6d9b36a997accc7d81b56d
mehdi557 <elmehdi.chbani@gmail.com>
2025-04-19 13:12:51 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  }
 })
